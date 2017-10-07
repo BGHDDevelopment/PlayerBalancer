16b05eb9d341106426b498fbe3e6f64fc6bd2efb
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2017-10-07 19:03:49 +0200

    Modules separating different variants

---
+++ Spigot Addon/src/main/java/com/jaimemartz/playerbalanceraddon/PlayerBalancerAddon.java
@@ -0,0 +1,15 @@
+package com.jaimemartz.playerbalanceraddon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PlayerBalancerAddon extends JavaPlugin {
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+}
--- src/test/java/GeneralTest.java
+++
@@ -1,8 +0,0 @@
-import org.junit.Test;
-
-public class GeneralTest {
-    @Test
-    public void test() {
-        //Test
-    }
-}
