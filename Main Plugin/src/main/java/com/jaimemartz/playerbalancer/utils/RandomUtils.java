e03e93b9b90f83788d43e00214243e7b81c04d2f
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2017-12-28 22:39:30 +0100

    Version 2.1.4.2 bump

--- Main Plugin/src/main/java/com/jaimemartz/playerbalancer/utils/RandomUtils.java
+++ Main Plugin/src/main/java/com/jaimemartz/playerbalancer/utils/RandomUtils.java
@@ -3,10 +3,12 @@ package com.jaimemartz.playerbalancer.utils;
 import java.security.SecureRandom;
 import java.util.List;
 
-public class RandomUtils {
+public final class RandomUtils {
     private static final SecureRandom instance = new SecureRandom();
 
     public static <T> T random(List<T> list) {
         return list.get(instance.nextInt(list.size()));
     }
+
+    private RandomUtils() {}
 }
