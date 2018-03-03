b19e51cf5636055f59e7aaa16ea329bb5da712d2
Jaime <mrjaime1999@gmail.com>
2018-03-03 16:12:34 +0100

    Fixed infinite queue of data to fake servers

--- Main Plugin/src/main/java/com/jaimemartz/playerbalancer/section/SectionServer.java
+++ Main Plugin/src/main/java/com/jaimemartz/playerbalancer/section/SectionServer.java
@@ -2,12 +2,21 @@ package com.jaimemartz.playerbalancer.section;
 
 import com.jaimemartz.playerbalancer.settings.props.features.BalancerProps;
 import net.md_5.bungee.BungeeServerInfo;
+import net.md_5.bungee.api.Callback;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.ServerPing;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
+import javax.xml.soap.Text;
+import java.awt.*;
 import java.net.InetSocketAddress;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Queue;
+import java.util.function.IntFunction;
 import java.util.stream.Collectors;
 
 public class SectionServer extends BungeeServerInfo {
@@ -34,5 +43,46 @@ public class SectionServer extends BungeeServerInfo {
                     .flatMap(Collection::stream)
                     .collect(Collectors.toList());
         } else return Collections.emptyList();
+    }
+
+    @Override
+    public void sendData(String channel, byte[] data) {
+        this.sendData(channel, data, true);
+    }
+
+    @Override
+    public boolean sendData(String channel, byte[] data, boolean queue) {
+        //Nothing to do
+        return true;
+    }
+
+    @Override
+    public void ping(Callback<ServerPing> callback) {
+        this.ping(callback, ProxyServer.getInstance().getProtocolVersion());
+    }
+
+    @Override
+    public void ping(Callback<ServerPing> callback, int protocolVersion) {
+        ServerPing ping = new ServerPing();
+
+        ping.setDescriptionComponent(new TextComponent(
+                TextComponent.fromLegacyText(this.getMotd())
+        ));
+
+        ping.setVersion(new ServerPing.Protocol(
+                ProxyServer.getInstance().getName(),
+                protocolVersion
+        ));
+
+        Collection<ProxiedPlayer> players = getPlayers();
+        ping.setPlayers(new ServerPing.Players(
+                Integer.MAX_VALUE,
+                players.size(),
+                players.stream().map(
+                        player -> new ServerPing.PlayerInfo(player.getName(), player.getUniqueId())
+                ).toArray(ServerPing.PlayerInfo[]::new)
+        ));
+
+        callback.done(ping, null);
     }
 }
